666fda23287baca86cd669fd0338880f63d201de
Akash5Chaurasiya <anitachaurasiya9936@gmail.com>
2023-12-18 15:44:31 +0530

    cors

--- index.ts
+++ index.ts
@@ -4,12 +4,15 @@ import skill from "./routes/skills/skills.route";
 import userRouter from "./routes/users/user.routes";
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const allowedOrigins = ['http://localhost:3000', 'https://bejewelled-khapse-aea2f6.netlify.app'];
-const express = require('express');
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://bejewelled-khapse-aea2f6.netlify.app',
+    'https://remote-front-akashs-projects-db31b022.vercel.app'
+]; const express = require('express');
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: '*', credentials: true }));
-app.options('*', cors({ origin: '*', credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.options('*', cors({ origin: allowedOrigins, credentials: true }));
 require('dotenv').config();
 app.use(cookieParser());
 app.use('/users', userRouter);
