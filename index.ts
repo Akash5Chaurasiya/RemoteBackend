b0f51f6171bebf21ba948a3f21b9155cd9625e82
Akash5Chaurasiya <anitachaurasiya9936@gmail.com>
2023-12-18 15:17:19 +0530

    parsed

--- index.ts
+++ index.ts
@@ -7,18 +7,21 @@ const cors = require('cors');
 
 const express = require('express');
 const app = express();
+
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
-app.options('*', cors({ origin: 'http://localhost:3000', credentials: true }));
-require('dotenv').config();
+app.use(cors({ origin: '*', credentials: true }));
+app.options('*', cors({ origin: '*', credentials: true }));
 app.use(cookieParser());
 app.use('/users', userRouter);
-app.use(`/skills`, skill)
+app.use('/skills', skill);
+
+require('dotenv').config();
+
 app.listen(process.env.PORT, async () => {
     try {
-        await connection
-        console.log("listening to port", process.env.PORT)
+        await connection;
+        console.log("Server listening on port", process.env.PORT);
     } catch (error) {
-        console.log(error);
+        console.error("Error starting the server:", error);
     }
-})
+});
