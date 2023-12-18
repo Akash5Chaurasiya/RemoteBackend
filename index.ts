0f748fdeb65161089754e9d42a0419a076f2cb71
Akash5Chaurasiya <anitachaurasiya9936@gmail.com>
2023-12-18 15:23:02 +0530

    d

--- index.ts
+++ index.ts
@@ -4,24 +4,22 @@ import skill from "./routes/skills/skills.route";
 import userRouter from "./routes/users/user.routes";
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-
+const allowedOrigins = ['http://localhost:3000', 'https://bejewelled-khapse-aea2f6.netlify.app'];
 const express = require('express');
 const app = express();
-
 app.use(express.json());
-app.use(cors({ origin: '*', credentials: true }));
-app.options('*', cors({ origin: '*', credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.options('*', cors({ origin: allowedOrigins, credentials: true }));
+require('dotenv').config();
 app.use(cookieParser());
 app.use('/users', userRouter);
-app.use('/skills', skill);
-
-require('dotenv').config();
-
+app.use(`/skills`, skill)
 app.listen(process.env.PORT, async () => {
     try {
-        await connection;
-        console.log("Server listening on port", process.env.PORT);
+        await connection
+        console.log("listening to port", process.env.PORT)
     } catch (error) {
-        console.error("Error starting the server:", error);
+        console.log(error);
     }
-});
+})
+
